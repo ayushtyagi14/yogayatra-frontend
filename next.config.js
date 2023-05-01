1c0cbe1a5cfb29e24b4cd0fbbea733b1723cbc22
Ayush Tyagi <ayush.tyagi145@gmail.com>
2023-05-01 17:32:49 +0530

    next config file updated

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  exportPathMap: async function (defaultPathMap) {
+    delete defaultPathMap['/event/[id]']
+    delete defaultPathMap['/events']
+    delete defaultPathMap['/session/[id]']
+    delete defaultPathMap['/sessions']
+    return defaultPathMap
+  },
 };
 
 module.exports = nextConfig;
