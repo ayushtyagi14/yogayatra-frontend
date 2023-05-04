f7480f38c304274c3e6efc6f6993ff73953f9ce3
Ayush Tyagi <ayush.tyagi145@gmail.com>
2023-05-05 00:32:15 +0530

    changes back to normal

--- next.config.js
+++ next.config.js
@@ -5,22 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  experimental: { esmExternals: true },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    // Add the following lines to include the required modules
-    config.resolve.alias['react-slick'] = 'react-slick';
-    config.resolve.alias['slick-carousel/slick/slick-theme.css'] = 'slick-carousel/slick/slick-theme.css';
-    config.resolve.alias['slick-carousel/slick/slick.css'] = 'slick-carousel/slick/slick.css';
-
-    return config
-  },
   exportPathMap: async function (defaultPathMap) {
     delete defaultPathMap['/event/[id]']
     delete defaultPathMap['/events']
--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,11 @@ import WhyYoga from '../components/Homepage/WhyYoga';
 import Training from '../components/Homepage/Training';
 import Class from '../components/Homepage/Class';
 import Team from '../components/Homepage/Team';
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Testimonial from '../components/Homepage/Testimonial';
+
+import { useEffect } from 'react';
 
 export default function Home() {
-
-  const [testimonials, setTestimonials] = useState({});
-
-  const getTestimonial = () => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch("https://yogayatra.in/api/admin/getAllTestimonials", requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
-        const data = JSON.parse(result);
-        setTestimonials(data.testimonials);
-      })
-      .catch((error) => console.log("error", error));
-  };
-
-  useEffect(() => {
-    getTestimonial();
-  }, []);
 
   return (
     <Layout>
@@ -46,49 +23,7 @@ export default function Home() {
       <Training />
       <Class />
       <Team />
-      <div className="mx-10 my-40">
-        <h1 className="md:text-[40px] text-[30px] text-center leading-none">
-          We Asked People What They Like <br /> About Our{" "}
-          <span className="text-[#f86454]">Yoga and Fitness</span>
-        </h1>
-        <Slider
-          dots={true}
-          infinite={true}
-          speed={500}
-          slidesToShow={1}
-          slidesToScroll={1}
-        >
-          {testimonials.length > 0 &&
-            testimonials.map((item) => (
-              <div key={item._id}>
-                <div className="bg-white text-black h-max min-h-[250px] mt-20 grid md:grid-cols-2 grid-cols-1 rounded-lg items-center w-[90%] mx-auto">
-                  <div className="flex justify-center md:justify-start">
-                    <Image
-                      src={item.testimonialImg}
-                      alt={item.testimonialPersonName}
-                      className="md:ml-20 p-5 h-[300px] object-cover"
-                      width={250}
-                      height={200}
-                    />
-                  </div>
-                  <div className="flex flex-col md:items-start items-center md:text-left text-center">
-                    <div className="my-5">
-                      <h1 className="text-[24px] uppercase">
-                        {item.testimonialPersonName}
-                      </h1>
-                      <p className="text-[#5c5c5c] my-2">
-                        {item.testimonialPersonDesig}
-                      </p>
-                    </div>
-                    <p className="text-[17px] md:text-left md:mx-0 mx-2">
-                      &apos;&apos; {item.testimonialContent} &apos;&apos;
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </Slider>
-      </div>
+      <Testimonial />
     </Layout>
   )
 }
